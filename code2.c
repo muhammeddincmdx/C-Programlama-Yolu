24656e632ee22cff2a8850218de9100b7d4a9222
Muhammed Dinç <54439858+muhammeddincmdx@users.noreply.github.com>
2024-02-21 21:31:36 +0300

    Create code2.c

---
+++ code2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+    
+    float km,miles;
+    miles = 10.0;
+    km = miles*1.609;
+    printf("%f miles is equal to %f kilometers ",miles,km);
+    
+}
+
+
+/*
+OUTPUT
+10.000000 miles is equal to 16.090000 kilometers 
+*/
